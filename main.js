aad851f3b1d89fd555d2b6ead2702f23b828024d
troemanes <mertunal1998@gmail.com>
2025-08-28 22:36:47 +0300

    second modal started

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
         "By checking this box, you agree to our Terms of Use and consent to our <b>Privacy Policy *<b>",
       buttonText: "Spin",
       winnerDiscount: 10,
-      whellDiscounts: [1, 5, 30, 55, 40, 20, 25, 99],
+      whellDiscounts: [1, 85, 30, 55, 40, 20, 25, 90],
       colors: [
         "#33C1FF",
         "#FF33A6",
@@ -34,7 +34,7 @@
       title: "YOU WON!",
       date: "20.01.2026",
       text: "Here is your discount code you can use in your next order. This coupon will be valid until 20.01.2026",
-      coupon: "",
+      coupon: "@dasdadsada",
       copyText: "Copy",
     },
   };
@@ -49,6 +49,17 @@
     discount: "mrt-discount-price",
     spinButton: "mrt-ins-spin-button",
     title: "mrt-ins-title",
+    mainModal: "mrt-main-modal",
+    overlay: "mrt-ins-overlay",
+    closeButton: "mrt-ins-close-button",
+
+    codeModalMain: "asdasda",
+    codeImg: "asdadadd",
+    codeTitle: "asdadasdd",
+    codeUpperText: "sdaşlsdalsdşasda",
+    codeTextDiv: "dsddccc",
+    codeArea: "adlaşlda",
+    codeCopyButton: "llfdşflsşdlfs",
   };
 
   const selectors = Object.keys(classes).reduce((createdSelector, key) => {
@@ -82,18 +93,67 @@
       discount,
       spinButton,
       title,
+      mainModal,
+      overlay,
+      closeButton,
+      codeModalMain,
+      codeImg,
+      codeTitle,
+      codeUpperText,
+      codeTextDiv,
+      codeArea,
+      codeCopyButton,
     } = selectors;
     const customStyle = `
     <style>
+
+
+      ${overlay}{
+      display:flex;
+     position: fixed;
+              top:0;
+              left:0;
+              width:100%;
+              height:100%;
+              background: rgba(0, 0, 0, 0.7);
+              z-index:9999;
+             align-items:center;
+             justify-content:center;
+              }
+            
+     ${closeButton}{
+      cursor:pointer;
+      border:none;
+      font-size:16px;
+      font-family:Arial;
+      background-color:white;
+      color:red;
+      font-weight:bold;
+      position:absolute;
+      top:10px;
+      right:10px;
+      margin:0;
+     }
+
+
+      ${mainModal}{
+          position: relative;
+      border:1px solid gray;
+      border-radius:10px;
+      background-color:white;
+      padding:10px 60px;
+      }
+
       ${spinMain} {
-        position: absolute;
+        position: fixed;
         border: 20px solid #0973d6ff;
         border-radius: 50%;
         height: 300px;
         width: 300px;
-        top: 10%;
-        left: 35%;
-       transform-origin: 0% 0%; 
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+
       }
 
       ${pizzaContent}{
@@ -103,23 +163,23 @@
       }
 
       ${pizza} {
-        position: absolute;
-        top: 30%;
-        left: 34%;
+        position: fixed ;
+        top: 28%;
+        left: 33%;
         width: 100px;
         height: 130px;
         background: orange;
         clip-path: polygon(0% 0%,50% 100%,100% 0%);
-        border-top-left-radius: 70%;
-        border-top-right-radius: 70%;
-        transform-origin: 50% 50%;
+        border-top-left-radius: 45%;
+        border-top-right-radius: 45%;
+        transform: translate(-50%,-50%);
       }
 
        ${stick}{
         clip-path: polygon(0% 0%,50% 100%,100% 0%);
          background-color:red;
          width:50px;
-         height:80px;
+         height:70px;
          border-radius:30px;
          color:black;
          position:absolute;
@@ -141,7 +201,6 @@
        ${title}{
         text-align:center;
         font-family:Arial;
-
        }
 
       ${emailDiv}{
@@ -170,19 +229,64 @@
 
         ${spinButton}{
           border:none;
-          padding:8px;
+          padding:12px;
           background: #048bfaff;
           color:white;
           font-size:18px;
           width:100%;
           border-radius:5px;
           font-weight:bold;
+          margin:30px 0px;          
         }
 
            ${spinButton}:hover{
            cursor:pointer;
            background: #0069beff;
            }
+
+          ${spinButton}:disabled{
+            cursor:default;
+            background: #737e88ff;
+          }
+
+
+      /*coupon Modal*/
+
+  ${codeModalMain}{
+     font-family:Arial
+     padding:10px;
+     background-color:white;
+      position: fixed;
+        border-radius: 50%;
+        height: 300px;
+        width: 300px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+         text-align:center;
+  }
+
+     ${codeTextDiv}{
+       display:flex;
+      flex-direction:row;
+
+     }
+ 
+
+     ${codeCopyButton}{
+       border:none;
+          background: #048bfaff;
+          color:white;
+          font-size:14px;
+          width:50%;
+          border-radius:5px;
+          font-weight:bold;
+     }
+   
+            ${codeCopyButton}:hover{
+              cursor:pointer;
+              background: #0069beff;
+            }
 
     </style>
   `;
@@ -200,6 +304,9 @@
       discount,
       spinButton,
       title,
+      mainModal,
+      overlay,
+      closeButton,
     } = classes;
     const pizzaCount = config.spinSection.whellDiscounts.length;
 
@@ -207,7 +314,7 @@
       const angle = i * (360 / pizzaCount);
       const color = config.spinSection.colors[i] || "orange";
       return `
-      <div class="${pizza}" style="transform: rotate(${angle}deg) translateY(-68px); background-color: ${color};">
+      <div class="${pizza}" style="transform: rotate(${angle}deg) translateY(-75px); background-color: ${color};">
       <p class="${discount}">${config.spinSection.whellDiscounts[i]}%</p>
       
       </div>`;
@@ -221,8 +328,13 @@
     </div>`;
 
     const html = `
+    <div class="${overlay}">
+   <div class="${mainModal}">
+    <button class="${closeButton}">${config.spinSection.closeButton}</button>
     <div style="justify-content:center; display:flex; flex-direction:column; align-items:center;">
-      ${carkifelek}
+        
+
+    ${carkifelek}
      <div style="margin-top:420px;">
       
       <h2 class="${title}">${config.spinSection.title}</h2>
@@ -240,28 +352,66 @@
          <button class="${spinButton}">${config.spinSection.buttonText}</button>
       </div>
      </div>
+     </div>
+    
      `;
     $("body").append(html);
+
+    const {
+      imgUrl,
+      title: codeTitleText,
+      date,
+      text,
+      coupon,
+      copyText,
+    } = config.codeSection;
+
+    const {
+      codeModalMain,
+      codeImg,
+      codeTitle,
+      codeUpperText,
+      codeTextDiv,
+      codeArea,
+      codeCopyButton,
+    } = classes;
+    const codeModal = `
+     <div class="${codeModalMain}">
+     
+     <img  class="${codeImg}" src="${imgUrl}" />
+      <h3 class="${codeTitle}" >${codeTitleText}</h3>
+      <p class="${codeUpperText}" >${text}</p>
+      <div class="${codeTextDiv}">
+        <p class="${codeArea}">${coupon}</p>
+        <button class="${codeCopyButton}" >${copyText}</button>
+      </div>
+     
+     
+     </div>
+     
+    `;
+
+    $("body").append(codeModal);
   };
 
   self.spinWheel = () => {
     const { pizzaContent } = selectors;
-    const wheel = document.querySelector(pizzaContent);
+    const $wheel = $(pizzaContent);
 
     let currentRotation = 0;
     let speed = 8;
     const friction = 0.998;
-    const minSpeed = 0.05;
+    const minSpeed = 0.1;
 
     const animate = () => {
       currentRotation += speed;
-      wheel.style.transform = `rotate(${currentRotation}deg)`;
+      $wheel.css("transform", `rotate(${currentRotation}deg)`);
       speed *= friction;
 
       if (speed > minSpeed) {
         requestAnimationFrame(animate);
       } else {
-        wheel.style.transform = `rotate(${currentRotation.toFixed(0)}deg)`;
+        $wheel.css("transform", `rotate(${currentRotation.toFixed(0)}deg)`);
       }
     };
 
@@ -269,8 +419,16 @@
   };
 
   self.events = () => {
-    $(selectors.spinButton).on("click", () => {
+    const { spinButton, closeButton } = selectors;
+
+    $(spinButton).on("click", () => {
       self.spinWheel();
+    });
+
+    $(closeButton).on("click", () => {
+      console.log("tıklandı");
+
+      $(".mainModal").remove();
     });
   };
   self.init();
